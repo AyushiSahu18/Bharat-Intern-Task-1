ede09403f1b287d5295d4521f65f83649df517c2
AyushiSahu18 <129952366+AyushiSahu18@users.noreply.github.com>
2023-08-20 16:38:20 +0530

    Task1.py

--- Task1.py
+++ Task1.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# ## Stock Prediction using LSTM 
-
-# In[1]:
-
+## Stock Prediction using LSTM 
 
 #Importing libraries required for this task
 import pandas as pd
@@ -17,77 +13,36 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import LSTM
 import tensorflow as tf
 
-
-# In[2]:
-
-
 #Importing the dataset of Teslastock
 df=pd.read_csv('Teslastock.csv')
-
-
-# In[3]:
-
 
 #Displaying the top two records
 df.head(2)
 
-
-# In[4]:
-
-
 #Displaying the bottom two records
 df.tail(2)
-
-
-# In[5]:
-
 
 #Arranging the data into ascending order
 data=df.reset_index()['High']
 data
 
-
-# In[6]:
-
-
 #Ploting the graph for High column
 plt.plot(data)
 
-
-# In[7]:
-
-
 #Printing data
 data
-
-
-# In[8]:
-
 
 #Reshaping the data
 scaler=MinMaxScaler(feature_range=(0,1))
 data=scaler.fit_transform(np.array(data).reshape(-1,1))
 data
 
-
-# In[9]:
-
-
 ##splitting dataset into train and test split
 training_size=int(len(data)*0.65)
 test_size=len(data)-training_size
 train_data,test_data=data[0:training_size,:],data[training_size:len(data),:1]
 training_size,test_size
-
-
-# In[10]:
-
-
 train_data
-
-
-# In[11]:
-
 
 import numpy
 # convert an array of values into a dataset matrix
@@ -99,10 +54,6 @@ def create_dataset(dataset, time_step=1):
         dataY.append(dataset[i + time_step, 0])
     return numpy.array(dataX), numpy.array(dataY)
 
-
-# In[12]:
-
-
 # reshape into X=t,t+1,t+2..t+99 and Y=t+100
 time_step = 100
 X_train, y_train = create_dataset(train_data, time_step)
@@ -110,17 +61,9 @@ X_test, ytest = create_dataset(test_data, time_step)
 print(X_train.shape), print(y_train.shape)
 print(X_test.shape), print(ytest.shape)
 
-
-# In[13]:
-
-
 # reshape input to be [samples, time steps, features] which is required for LSTM
 X_train =X_train.reshape(X_train.shape[0],X_train.shape[1] , 1)
 X_test = X_test.reshape(X_test.shape[0],X_test.shape[1] , 1)
-
-
-# In[14]:
-
 
 model=Sequential()
 model.add(LSTM(50,return_sequences=True,input_shape=(100,1)))
@@ -129,59 +72,25 @@ model.add(LSTM(50))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error',optimizer='adam')
 
-
-# In[15]:
-
-
 model.summary()
-
-
-# In[16]:
-
-
-model.summary()
-
-
-# In[17]:
-
 
 model.fit(X_train,y_train,validation_data=(X_test,ytest),epochs=100,batch_size=64,verbose=1)
-
-
-# In[ ]:
-
 
 ### Lets Do the prediction and check performance metrics
 train_predict=model.predict(X_train)
 test_predict=model.predict(X_test)
 
-
-# In[19]:
-
-
 ##Transformback to original form
 train_predict=scaler.inverse_transform(train_predict)
 test_predict=scaler.inverse_transform(test_predict)
-
-
-# In[20]:
-
 
 ### Calculate RMSE performance metrics
 import math
 from sklearn.metrics import mean_squared_error
 math.sqrt(mean_squared_error(y_train,train_predict))
 
-
-# In[21]:
-
-
 ### Test Data RMSE
 math.sqrt(mean_squared_error(ytest,test_predict))
-
-
-# In[23]:
-
 
 ### Plotting 
 # shift train predictions for plotting
@@ -197,36 +106,18 @@ testPredictPlot[len(train_predict)+(look_back*2)+1:len(data)-1, :] = test_predic
 plt.plot(scaler.inverse_transform(data))
 plt.plot(trainPredictPlot)
 plt.plot(testPredictPlot)
-plt.show()
-
-
-# In[24]:
-
 
 len(test_data)
 
-
-# In[25]:
-
-
 x_input=test_data[341:].reshape(1,-1)
 x_input.shape
-
-
-# In[26]:
-
 
 temp_input=list(x_input)
 temp_input=temp_input[0].tolist()
 temp_input
 
-
-# In[27]:
-
-
 # demonstrate prediction for next 30 days
 from numpy import array
-
 lst_output=[]
 n_steps=100
 i=0
@@ -253,50 +144,21 @@ while(i<30):
         temp_input.extend(yhat[0].tolist())
         print(len(temp_input))
         lst_output.extend(yhat.tolist())
-        i=i+1
-    
-
+        i=i+1   
 print(lst_output)
-
-
-# In[28]:
-
 
 day_new=np.arange(1,101)
 day_pred=np.arange(101,131)
-
-
-# In[29]:
-
-
 len(data)
 
-
-# In[30]:
-
-
 scaler.inverse_transform(lst_output)
-
-
-# In[31]:
-
-
 plt.plot(day_new,scaler.inverse_transform(data[1158:]))
 plt.plot(day_pred,scaler.inverse_transform(lst_output))
 plt.savefig('30daypredict.png')
-
-
-# In[32]:
-
 
 data1=data.tolist()
 data1.extend(lst_output)
 plt.plot(data1[1200:])
 
-
-# In[33]:
-
-
 data1=scaler.inverse_transform(data1).tolist()
 plt.plot(data1)
-
